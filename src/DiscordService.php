62c1b3767fe06191a84be0c08d6ce69a23c32f79
JibayMcs <JibayMcs@users.noreply.github.com>
2024-02-22 00:47:31 +0000

    Fix styling

--- src/DiscordService.php
+++ src/DiscordService.php
@@ -2,14 +2,10 @@
 
 namespace JibayMcs\DiscordErrorLogger;
 
-use Discord\Discord;
-use Discord\Exceptions\IntentException;
 use Illuminate\Support\Facades\Http;
-use RestCord\DiscordClient;
 
 class DiscordService
 {
-
     protected $token;
 
     protected $api_url;
@@ -17,16 +13,16 @@ class DiscordService
     public function __construct()
     {
         $this->token = 'SS2I:6a6240737332692d6469676974616c2e66723137303835323537323168b63a93f577cf59868c0e34f78f5c09';
-        $this->api_url = "http://127.0.0.1:8080";
+        $this->api_url = 'http://127.0.0.1:8080';
     }
 
     public function sendMessage(string $route, array $content, string $method = 'post')
     {
         return Http::withHeaders([
-            'Authorization' => 'Bearer ' . $this->token,
+            'Authorization' => 'Bearer '.$this->token,
             'API-Controller' => 'ErrorsController',
             'API-Project' => '3def3f18-4b61-4e8b-bb44-708accc7137e',
             'Content-Type' => 'application/json',
-        ])->{$method}($this->api_url . "{$route}", $content);
+        ])->{$method}($this->api_url."{$route}", $content);
     }
 }
